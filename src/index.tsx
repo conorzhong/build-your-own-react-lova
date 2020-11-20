3f2ddb21c3eb79f1cdd301488c9faf774ed65d77
conor <39456642+conorzhong@users.noreply.github.com>
2020-11-20 11:27:56 +0800

    Step Zero: Review

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
+type ReactElement = {
+  type: string;
+  props: {
+    title: string;
+    children: string;
+  };
+};
 
-const element = <h1 title="foo">Hello</h1>
-const container = document.getElementById("root")
-ReactDOM.render(element, container)
+// const element = <h1 title="foo">Hello</h1>
+const element: ReactElement = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "Hello"
+  }
+};
+
+const container = document.getElementById("root");
+
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+
+node.appendChild(text);
+container?.appendChild(node);
