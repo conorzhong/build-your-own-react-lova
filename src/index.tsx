e9f09a195a1bfe3c2dbe1cd1bb5dcca961972627
conor <39456642+conorzhong@users.noreply.github.com>
2020-11-25 21:52:31 +0800

    Step VI: Reconciliation

--- src/index.tsx
+++ src/index.tsx
@@ -39,28 +39,86 @@ function createDom(fiber: Fiber) {
   return dom;
 }
 
+const isEvent = (key: string) => key.startsWith("on");
+const isProperty = (key: string) => key !== "children" && !isEvent(key);
+const isNew = (prev: object, next: object) => (key: string) =>
+  prev[key] !== next[key];
+const isGone = (prev: object, next: object) => (key: string) => !(key in next);
+function updateDom(
+  dom: HTMLElement | Text,
+  prevProps: Fiber["props"],
+  nextProps: Fiber["props"]
+) {
+  //Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = "";
+    });
+
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = nextProps[name];
+    });
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+}
+
 function commitRoot() {
+  // And then, when we are commiting the changes to the DOM, we also use the fibers from that array.
+  deletions!.forEach(commitWork);
   // add nodes(the whole fiber tree) to dom
-  commitWork(wipRoot!.child) // 调用的时候判断了 wipRoot 非空，注意这里是 child
-  wipRoot = null
+  commitWork(wipRoot!.child); // 调用的时候判断了 wipRoot 非空，注意这里是 child
+  currentRoot = wipRoot;
+  wipRoot = null;
 }
 function commitWork(fiber?: Fiber) {
-  if(!fiber){
+  if (!fiber) {
     return;
   }
-  const domParent = fiber.parent!.dom!;  // 因为是 wipRoot.child 且非 root fiber 都有 parent，构建完 fiber tree 后都有 dom
+  const domParent = fiber.parent!.dom!; // 因为是 wipRoot.child 且非 root fiber 都有 parent，构建完 fiber tree 后都有 dom
+  if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate?.props, fiber.props);
+  } else if (fiber.effectTag === "DELETION") {
+    domParent.removeChild(fiber.dom);
+  }
   domParent.appendChild(fiber.dom!);
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 }
 
 function render(element: Fiber, container: HTMLElement | Text) {
-  wipRoot = { // keep track of the root of the fiber tree.
+  wipRoot = {
+    // keep track of the root of the fiber tree.
     dom: container,
     props: {
       children: [element]
-    }
+    },
+    alternate: currentRoot
   };
+  deletions = [];
   nextUnitOfWork = wipRoot;
 }
 
@@ -75,9 +133,13 @@ type Fiber = {
   parent?: Fiber;
   child?: Fiber;
   sibling?: Fiber;
+  alternate?: Fiber | null; // This property is a link to the old fiber, the fiber that we committed to the DOM in the previous commit phase.
+  effectTag?: string;
 };
 let nextUnitOfWork: Fiber | null | undefined = null;
-let wipRoot: Fiber | null = null
+let wipRoot: Fiber | null = null;
+let currentRoot: Fiber | null = null; // last fiber tree we committed to the DOM
+let deletions: Fiber[] | null = null;
 
 function workLoop(deadline: any) {
   let shouldYield = false;
@@ -103,27 +165,7 @@ function performUnitOfWork(fiber: Fiber): Fiber | undefined {
 
   // create new fibers
   const elements = fiber.props.children;
-  let index = 0;
-  let prevSibling = null;
-
-  while (index < elements.length) {
-    const element = elements[index];
-    const newFiber: Fiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null
-    };
-
-    if (index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling!.sibling = newFiber; // 第0次循环，prevSibling 会被初始化
-    }
-
-    prevSibling = newFiber;
-    index++;
-  }
+  reconcileChildren(fiber, elements);
 
   // return next unit of work
   if (fiber.child) {
@@ -135,6 +177,64 @@ function performUnitOfWork(fiber: Fiber): Fiber | undefined {
       return nextFiber.sibling;
     }
     nextFiber = nextFiber.parent;
+  }
+}
+
+// Here we will reconcile the old fibers with the new elements.
+// The element is the thing we want to render to the DOM and the oldFiber is what we rendered the last time.
+function reconcileChildren(wipFiber: Fiber, elements: Fiber[]) {
+  // create new fibers
+  let index = 0;
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+  let prevSibling = null;
+
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    let newFiber: Fiber | null = null;
+
+    // compare oldFiber to element
+    const sameType = oldFiber && element && element.type == oldFiber.type;
+
+    if (sameType) {
+      // update the node
+      newFiber = {
+        type: oldFiber!.type,
+        props: element.props,
+        dom: oldFiber!.dom,
+        parent: wipFiber,
+        alternate: oldFiber!,
+        effectTag: "UPDATE"
+      };
+    }
+    if (element && !sameType) {
+      // add this node
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT"
+      };
+    }
+    if (oldFiber && sameType) {
+      // delete the oldFiber's node
+      oldFiber.effectTag = "DELETION";
+      deletions!.push(oldFiber);
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber!;
+    } else {
+      prevSibling!.sibling = newFiber!; // 第0次循环，prevSibling 会被初始化
+    }
+
+    prevSibling = newFiber;
+    index++;
   }
 }
 
